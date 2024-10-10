2c0049c685a8c19849ed087afdafad90794c39d3
Theluther101 <afolabi12356@gmail.com>
2024-10-10 15:54:22 +0000

    not done

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,15 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+// export function Home() {
+//   const [ selectedColor, setSelectedColor ] = useState(selectedRed)
+//   return (
+//     <div className="traffic-light">
+//       <div className= {selectedColor ==="red" ? "red selectedRed" : "red"} onClick{() => setSelectedColor("red")}></div>
+//       <div className="light yellow"></div>
+//       <div className="light green"></div>
+//     </div>
+//   );
+// }
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.createRoot(document.getElementById("app")).render(<Home />);
